21aa48cf34ca28037e29b139c51cbd74ed3a2e11
Chaudhry Faizan Mustansar <fmustansar@gmail.com>
2020-09-27 23:45:41 +0200

    Getting plot set for actions on the Board component

---
+++ src/index.js
@@ -0,0 +1,91 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+class Square extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     value: null,
+  //   };
+  // }
+  render() {
+    return (
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.faizan}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+  renderSquare(i) {
+    console.log(i);
+    return (
+      <Square
+        faizan={this.state.squares[i]}
+        onClick={() => {
+          this.handleClick(i);
+        }}
+      />
+    );
+  }
+
+  handleClick(index) {
+    /* FM: Creating copy of the squares array by using slice */
+    const squares = this.state.squares.slice();
+    squares[index] = "X";
+    this.setState({ squares: squares });
+  }
+
+  render() {
+    const status = "Next player: X";
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById("root"));
