30c55927a7b54609889337396ecc4926a336b779
Ch Faizan Mustansar <fmustansar@gmail.com>
2020-10-01 23:24:33 +0200

    Writing my first 'FUNCTIONAL Component' by changing the Square from class to a function (Remember, remove all 'this' instances)

--- src/index.js
+++ src/index.js
@@ -2,20 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-class Square extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     value: null,
-  //   };
-  // }
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.faizan}
-      </button>
-    );
-  }
+// class Square extends React.Component {
+//   // constructor(props) {
+//   //   super(props);
+//   //   this.state = {
+//   //     value: null,
+//   //   };
+//   // }
+//   render() {
+//     return (
+//       <button className="square" onClick={() => this.props.onClick()}>
+//         {this.props.faizan}
+//       </button>
+//     );
+//   }
+// }
+
+function Square(props) {
+  return (
+    <button className="square" onClick={() => this.props.onClick()}>
+      {props.faizan}
+    </button>
+  );
 }
 
 class Board extends React.Component {
